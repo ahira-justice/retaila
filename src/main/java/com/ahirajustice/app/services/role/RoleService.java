9c50c298c522c2cf60088451f33fd8f8a0bad8b3
Ahira Adefokun <justiceahira@gmail.com>
2021-12-29 11:11:24 +0100

    Use mapping library

--- src/main/java/com/ahirajustice/app/services/role/RoleService.java
+++ src/main/java/com/ahirajustice/app/services/role/RoleService.java
@@ -1,11 +1,5 @@
 package com.ahirajustice.app.services.role;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-
 import com.ahirajustice.app.dtos.role.RoleCreateDto;
 import com.ahirajustice.app.dtos.role.RoleUpdateDto;
 import com.ahirajustice.app.entities.Permission;
@@ -13,41 +7,38 @@ import com.ahirajustice.app.entities.Role;
 import com.ahirajustice.app.exceptions.BadRequestException;
 import com.ahirajustice.app.exceptions.ForbiddenException;
 import com.ahirajustice.app.exceptions.NotFoundException;
+import com.ahirajustice.app.mappings.role.RoleMappings;
 import com.ahirajustice.app.repositories.IPermissionRepository;
 import com.ahirajustice.app.repositories.IRoleRepository;
 import com.ahirajustice.app.security.PermissionsProvider;
 import com.ahirajustice.app.services.permission.IPermissionValidatorService;
 import com.ahirajustice.app.viewmodels.role.RoleViewModel;
-
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
+@RequiredArgsConstructor
 public class RoleService implements IRoleService {
 
-    @Autowired
-    IRoleRepository roleRepository;
+    private final IRoleRepository roleRepository;
+    private final IPermissionRepository permissionRepository;
+    private final IPermissionValidatorService permissionValidatorService;
+    private final RoleMappings mappings;
 
-    @Autowired
-    IPermissionRepository permissionRepository;
-
-    @Autowired
-    IPermissionValidatorService permissionValidatorService;
 
     public List<RoleViewModel> getRoles() throws ForbiddenException {
         if (!permissionValidatorService.authorize(PermissionsProvider.CAN_VIEW_ALL_ROLES)) {
             throw new ForbiddenException();
         }
 
-        List<RoleViewModel> responses = new ArrayList<RoleViewModel>();
+        List<RoleViewModel> responses = new ArrayList<>();
 
         Iterable<Role> roles = roleRepository.findAll();
 
         for (Role role : roles) {
-            RoleViewModel response = new RoleViewModel();
-            BeanUtils.copyProperties(role, response);
-            responses.add(response);
+            responses.add(mappings.roleToRoleViewModel(role));
         }
 
         return responses;
@@ -58,17 +49,13 @@ public class RoleService implements IRoleService {
             throw new ForbiddenException();
         }
 
-        RoleViewModel response = new RoleViewModel();
-
         Optional<Role> roleExists = roleRepository.findById(id);
 
         if (!roleExists.isPresent()) {
             throw new NotFoundException(String.format("Role with id: '%d' does not exist", id));
         }
 
-        BeanUtils.copyProperties(roleExists.get(), response);
-
-        return response;
+        return mappings.roleToRoleViewModel(roleExists.get());
     }
 
     @Override
@@ -77,39 +64,30 @@ public class RoleService implements IRoleService {
             throw new ForbiddenException();
         }
 
-        RoleViewModel response = new RoleViewModel();
-
-        Role role = new Role();
-
         Optional<Role> roleExists = roleRepository.findByName(roleDto.getName());
 
         if (roleExists.isPresent()) {
             throw new BadRequestException(String.format("Role with name: '%s' already exists", roleDto.getName()));
         }
 
-        Set<Permission> permissions = new HashSet<Permission>();
+        Set<Permission> permissions = new HashSet<>();
         for (long permissionId : roleDto.getPermissionIds()) {
-            permissions.add(permissionRepository.findById(permissionId).get());
+            permissions.add(permissionRepository.findById(permissionId).orElse(null));
         }
 
-        BeanUtils.copyProperties(roleDto, role);
+        Role role = mappings.roleCreateDtoToRole(roleDto);
         role.setPermissions(permissions);
 
         Role createdRole = roleRepository.save(role);
 
-        BeanUtils.copyProperties(createdRole, response);
-
-        return response;
+        return mappings.roleToRoleViewModel(createdRole);
     }
 
     @Override
-    public RoleViewModel updateRole(RoleUpdateDto roleDto)
-            throws BadRequestException, ForbiddenException, NotFoundException {
+    public RoleViewModel updateRole(RoleUpdateDto roleDto) throws BadRequestException, ForbiddenException, NotFoundException {
         if (!permissionValidatorService.authorize(PermissionsProvider.CAN_UPDATE_ROLE)) {
             throw new ForbiddenException();
         }
-
-        RoleViewModel response = new RoleViewModel();
 
         Optional<Role> roleExists = roleRepository.findById(roleDto.getId());
 
@@ -117,11 +95,17 @@ public class RoleService implements IRoleService {
             throw new NotFoundException(String.format("Role with id: '%d' does not exist", roleDto.getId()));
         }
 
+        Optional<Role> roleNameExists = roleRepository.findByName(roleDto.getName());
+
+        if (roleNameExists.isPresent() && roleNameExists.get().getId() != roleDto.getId()){
+            throw new BadRequestException(String.format("Role with name: '%s' already exists", roleDto.getName()));
+        }
+
         Role role = roleExists.get();
 
-        Set<Permission> permissions = new HashSet<Permission>();
+        Set<Permission> permissions = new HashSet<>();
         for (long permissionId : roleDto.getPermissionIds()) {
-            permissions.add(permissionRepository.findById(permissionId).get());
+            permissions.add(permissionRepository.findById(permissionId).orElse(null));
         }
 
         role.setName(roleDto.getName());
@@ -129,9 +113,7 @@ public class RoleService implements IRoleService {
 
         Role updatedRole = roleRepository.save(role);
 
-        BeanUtils.copyProperties(updatedRole, response);
-
-        return response;
+        return mappings.roleToRoleViewModel(updatedRole);
     }
 
 }
