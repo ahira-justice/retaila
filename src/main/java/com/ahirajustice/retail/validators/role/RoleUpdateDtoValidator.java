0c9a74c78e485d640f187a0279dce74f52b818fb
Ahira Adefokun <justiceahira@gmail.com>
2022-02-05 19:43:06 +0100

    See 4f05e4c0

--- src/main/java/com/ahirajustice/retail/validators/role/RoleUpdateDtoValidator.java
+++ src/main/java/com/ahirajustice/retail/validators/role/RoleUpdateDtoValidator.java
@@ -3,7 +3,6 @@ package com.ahirajustice.retail.validators.role;
 import static br.com.fluentvalidator.predicate.LogicalPredicate.not;
 import static br.com.fluentvalidator.predicate.CollectionPredicate.empty;
 import static br.com.fluentvalidator.predicate.StringPredicate.stringEmptyOrNull;
-import static br.com.fluentvalidator.predicate.ComparablePredicate.greaterThan;
 
 import com.ahirajustice.retail.common.CommonHelper;
 import com.ahirajustice.retail.dtos.role.RoleUpdateDto;
@@ -14,8 +13,6 @@ public class RoleUpdateDtoValidator extends AbstractValidator<RoleUpdateDto> {
 
     @Override
     public void rules() {
-        ruleFor(RoleUpdateDto::getId)
-            .must(greaterThan(0l)).withMessage("id is required").withFieldName("id");
         ruleFor(RoleUpdateDto::getName)
             .must(not(stringEmptyOrNull())).withMessage("name is required").withFieldName("name")
             .must(this::nameIsUppercase).withMessage("name must be uppercase").withFieldName("name")
