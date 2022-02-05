c2dc41f86d872dcbc1e98634c2dfd2366dfa0d6f
Ahira Adefokun <justiceahira@gmail.com>
2022-02-05 19:59:28 +0100

    Modify application config values

--- src/main/java/com/ahirajustice/retail/config/AppConfig.java
+++ src/main/java/com/ahirajustice/retail/config/AppConfig.java
@@ -6,27 +6,27 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppConfig {
 
-    @Value("${app.config.accesstokenexpireminutes}")
+    @Value("${app.config.access-token-expire-minutes}")
     public int ACCESS_TOKEN_EXPIRE_MINUTES;
 
-    @Value("${app.config.secretkey}")
+    @Value("${app.config.secret-key}")
     public String SECRET_KEY;
 
     @Value("${app.config.superuser.email}")
     public String SUPERUSER_EMAIL;
 
-    @Value("${app.config.superuser.firstname}")
+    @Value("${app.config.superuser.first-name}")
     public String SUPERUSER_FIRST_NAME;
 
-    @Value("${app.config.superuser.lastname}")
+    @Value("${app.config.superuser.last-name}")
     public String SUPERUSER_LAST_NAME;
 
     @Value("${app.config.superuser.password}")
     public String SUPERUSER_PASSWORD;
 
-    @Value("${app.config.usertoken.length}")
+    @Value("${app.config.user-token.length}")
     public int USER_TOKEN_LENGTH;
 
-    @Value("${app.config.usertoken.keyspace}")
+    @Value("${app.config.user-token.keyspace}")
     public String USER_TOKEN_KEYSPACE;
 }
