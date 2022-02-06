481ef9c17264f23b15e985676db984d28f6e7106
Ahira Adefokun <justiceahira@gmail.com>
2022-02-06 14:47:36 +0100

    Fix RoleViewModel to use PermissionViewModel instead of Permission

--- src/main/java/com/ahirajustice/retail/viewmodels/role/RoleViewModel.java
+++ src/main/java/com/ahirajustice/retail/viewmodels/role/RoleViewModel.java
@@ -3,14 +3,14 @@ package com.ahirajustice.retail.viewmodels.role;
 import java.util.HashSet;
 import java.util.Set;
 
-import com.ahirajustice.retail.entities.Permission;
 import com.ahirajustice.retail.viewmodels.BaseViewModel;
+import com.ahirajustice.retail.viewmodels.permission.PermissionViewModel;
 
 public class RoleViewModel extends BaseViewModel {
 
     private String name;
 
-    private Set<Permission> permissions = new HashSet<Permission>();
+    private Set<PermissionViewModel> permissions = new HashSet<PermissionViewModel>();
 
     public String getName() {
         return name;
@@ -20,11 +20,11 @@ public class RoleViewModel extends BaseViewModel {
         this.name = name;
     }
 
-    public Set<Permission> getPermissions() {
+    public Set<PermissionViewModel> getPermissions() {
         return permissions;
     }
 
-    public void setPermissions(Set<Permission> permissions) {
+    public void setPermissions(Set<PermissionViewModel> permissions) {
         this.permissions = permissions;
     }
 
